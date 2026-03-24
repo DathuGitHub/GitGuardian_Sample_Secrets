29b4a423c1e6e2ba31968c9cbefd8261627ceb11
DathuGitHub <43470553+DathuGitHub@users.noreply.github.com>
2026-03-24 19:51:20 +0530

    Update MyTestFile.sh

--- MyTestFile.sh
+++ MyTestFile.sh
@@ -1,2 +1,31 @@
-Username - username
-Password = password
+username= 'username'
+password= 'password'
+test='4FV4TGjHr5t4d4eFBv75645C'
+password="tes456ti"
+password="MyPassword123"
+mypassword="mypassword"
+print ('Here is my password: '+str(password))
+jira_api_token='8EV4JKjHr5t4d4eFBv75692J'
+my_password = "paastswodrd@123"
+atlassian_api_='2J8PJKjHr5t4d4eFBv75608I'
+mypwd = "Chan$rahas1"
+ 
+curl -k $URLPATCH \
+-H 'client_id: 14c2a4g6523b4934fd2458cd1d42179f' \
+-H 'client_secret: 18GbaaB7E1a5448f96F3aE95206b96e4'
+
+SFTPPARM1=`echo "curl -v -Q \"site blksize=27800 CY lrecl=200\" -T "`
+echo "$SFTPPARM1"
+SFTPPARM2="$CDEOUT_MF"
+echo "$SFTPPARM2"
+SFTPPARM3=" "
+echo "$SFTPPARM3"
+SFTPPARM4=`echo "\"ftp://ecdfdfsb:adsfer@asdf.test.do.com/"`
+echo "$SFTPPARM4"
+SFTPPARM5=`echo "'$IFT_FILE_NAME'\""`
+echo "$SFTPPARM5"
+SFTPPARM6=" -k --ftp-ssl"
+echo "$SFTPPARM6"
+RUNCMD="$FTPPARM1$FTPPARM2$FTPPARM3$FTPPARM4$FTPPARM5$FTPPARM6"
+ 
+echo "$RUNCMDD"
