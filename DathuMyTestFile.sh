0c588308f6829ec90d5171043e3b0e597e43ff99
DathuGitHub <43470553+DathuGitHub@users.noreply.github.com>
2026-03-25 22:46:28 +0530

    Update DathuMyTestFile.sh

--- DathuMyTestFile.sh
+++ DathuMyTestFile.sh
@@ -9,7 +9,7 @@ mypassword="MyPasswords"
 print ('Here is my password: '+str(password))
 jira_api_token='8EV4JKjHr5t4d4eFBv75692JK'
 atlassian_api_='2J8PJKjHr5t4d4eFBv75608IJK'
- 
+key=git@github.com:DathuGitHub/GitGuardian_Sample_Secrets.git
 curl -k $URLPATCH \
 -H 'client_id: 14c2a4g6523b4934fd2458cd1d42179f' \
 -H 'client_secret: 18GbaaB7E1a5448f96F3aE95206b96e4'
